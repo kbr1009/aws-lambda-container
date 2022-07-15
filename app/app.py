3197cbed1cecdbc8a7245b064a4b7367c4fced4f
kbr1009 <reo109r@icloud.com>
2022-07-15 22:58:14 +0900

    lambda関数にhttpmethodによる条件分岐を追加した。

--- app/app.py
+++ app/app.py
@@ -1,21 +1,31 @@
 import json
 
-def handler(event, context):
-    try:
-        return{
-                "isBase64Encoded":False,
-                "statusCode":200,
-                "headers": {
-                    'Content-Type': 'application/json',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Origin': '*'
-                },
-            "body": json.dumps({"status":"succeed!"})
-        }
 
-    except Exception as e:
-        print(e)
-        res = {"status":"500"}
-        return json.dumps(res)
+def respond(err, res=None):
+    return{
+    "isBase64Encoded":False,
+    "statusCode":400 if err else 200,
+    "headers": {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "Content-Type",
+        'Access-Control-Allow-Methods': "GET,POST,PUT,DELETE",
+        'Access-Control-Allow-Origin': '*'
+        },
+    "body": err.message if err else json.dumps(res)
+    }
+
+
+
+def handler(event, context):
+    if event['httpMethod'] == 'GET':
+        try:
+            payload = "fuck!!!!!!"
+            return respond(None, payload)
+
+
+        except Exception as e:
+            res = {"status":"500"}
+            return respond(ValueError("no"))
+    else:
+        return respond(ValueError("no"))
 
