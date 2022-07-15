76783a3fb4d71942df4389a0c9f18e51abd2eed5
kbr1009 <reo109r@icloud.com>
2022-07-15 20:01:05 +0900

    upgrade

--- app/app.py
+++ app/app.py
@@ -2,8 +2,17 @@ import json
 
 def handler(event, context):
     try:
-        res = {"status":"200"}
-        return json.dumps(res)
+        return{
+                "isBase64Encoded":False,
+                "statusCode":200,
+                "headers": {
+                    'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Origin': '*'
+                },
+            "body": json.dumps({"status":"succeed!"})
+        }
 
     except Exception as e:
         print(e)
